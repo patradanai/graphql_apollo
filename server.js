e6dc463a1c4d8a79d0cf5c184fe44d0fc09f186e
Patradanai Nakpimay <bloody_jojo@hotmail.com>
2020-06-04 15:17:28 +0700

    add sequelize

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const { ApolloServer, gql } = require("apollo-server-express");
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("MT700PDDB", "sa", "qwerty@1", {
+  host: "172.16.73.146",
+  port: "1433", // <----------------The port number you copied
+  dialect: "mssql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  dialectOptions: {
+    options: {
+      enableArithAbort: true,
+      trustServerCertificate: true,
+    },
+  },
+});
+
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (err) {
+  console.error("Unable to connect to the database:", error);
+}
 
 // Some fake data
 const books = [
